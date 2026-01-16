cccbb6bada1980bd6925bb780994a922213c08d1
Ricardo Mardelli <ricardomardelli39@gmail.com>
2026-01-16 14:12:32 -0400

    ajuste traduccion

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,9 +1,11 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="es" translate="no">
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="google" content="notranslate">
+    <meta http-equiv="Content-Language" content="es">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title inertia>{{ config('app.name', 'Escaner Total') }}</title>
 
